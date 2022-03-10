2e8b354b71fe6c98cc7131ba2d08e6804c647d95
elitsuri <zelitesuri@gmail.com>
2022-03-10 13:27:33 +0200

    10/03/2022 13:27

--- vnc/Form1.Designer.cs
+++ vnc/Form1.Designer.cs
@@ -33,22 +33,25 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 181);
+            this.button1.Location = new System.Drawing.Point(62, 264);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 44);
+            this.button1.Size = new System.Drawing.Size(99, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "כניסה";
+            this.button1.Text = "ויקטורי";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(434, 152);
+            this.label1.Location = new System.Drawing.Point(493, 149);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 1;
@@ -57,7 +60,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(451, 208);
+            this.label2.Location = new System.Drawing.Point(510, 208);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 2;
@@ -65,24 +68,57 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(305, 149);
+            this.textBox1.Location = new System.Drawing.Point(383, 142);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(305, 205);
+            this.textBox2.Location = new System.Drawing.Point(383, 201);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "מחסני השוק";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(326, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 42);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "ברקת";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(471, 264);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 42);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "ח.כהן";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::vnc.Properties.Resources._93940963_148844160003112_4374142595740205056_n;
-            this.ClientSize = new System.Drawing.Size(722, 284);
+            this.ClientSize = new System.Drawing.Size(758, 343);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -103,6 +139,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- vnc/Form1.cs
+++ vnc/Form1.cs
@@ -21,7 +21,7 @@ namespace vnc
         {
             string branch = textBox1.Text.TrimStart().TrimEnd();
             string position = "10" + textBox2.Text.TrimStart().TrimEnd();
-            branch = convertBranch(branch);         
+            branch = convertBranchVictory(branch);
             ProcessStartInfo procStartInfo = new ProcessStartInfo("C:\\Program Files (x86)\\UltraVNC\\vncviewer.exe", "172.31." + branch + "." + position);
 
             using (Process process = new Process())
@@ -31,7 +31,7 @@ namespace vnc
                 process.WaitForExit();
             }
         }
-        private string convertBranch(string branch)
+        private string convertBranchVictory(string branch)
         {
             if (branch == "1") branch = "74";
             else if (branch == "2") branch = "22";
@@ -93,6 +93,143 @@ namespace vnc
             else if (branch == "95") branch = "62";
             else if (branch == "96") branch = "75";
 
+            return branch;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string branch = textBox1.Text.TrimStart().TrimEnd();
+            string position = "10" + textBox2.Text.TrimStart().TrimEnd();
+            branch = convertBranchMshuk(branch);
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("C:\\Program Files (x86)\\UltraVNC\\vncviewer.exe", "172.30." + branch + "." + position);
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = procStartInfo;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        private string convertBranchMshuk(string branch)
+        {
+            if (branch == "1") branch = "24";
+            else if (branch == "11") branch = "51";
+            else if (branch == "15") branch = "143";
+            else if (branch == "20") branch = "20";
+            else if (branch == "26") branch = "28";
+            else if (branch == "30") branch = "150";
+            else if (branch == "35") branch = "35";
+            else if (branch == "40") branch = "19";
+            else if (branch == "45") branch = "29";
+            else if (branch == "50") branch = "18";
+            else if (branch == "51") branch = "5";
+            else if (branch == "52") branch = "4";
+            else if (branch == "55") branch = "144";
+            else if (branch == "56") branch = "56";
+            else if (branch == "58") branch = "6";
+            else if (branch == "59") branch = "7";
+            else if (branch == "60") branch = "22";
+            else if (branch == "65") branch = "158";
+            else if (branch == "66") branch = "140";
+            else if (branch == "71") branch = "149";
+            else if (branch == "72") branch = "81";
+            else if (branch == "75") branch = "148";
+            else if (branch == "76") branch = "151";
+            else if (branch == "77") branch = "162";
+            else if (branch == "80") branch = "27";
+            else if (branch == "81") branch = "142";
+            else if (branch == "91") branch = "80";
+            else if (branch == "97") branch = "1";
+            else if (branch == "98") branch = "159";
+            else if (branch == "99") branch = "16";
+            else if (branch == "202") branch = "100";
+            else if (branch == "203") branch = "101";
+            else if (branch == "205") branch = "103";
+            else if (branch == "207") branch = "104";
+            else if (branch == "208") branch = "105";
+            else if (branch == "211") branch = "106";
+            else if (branch == "212") branch = "107";
+            else if (branch == "217") branch = "108";
+            else if (branch == "218") branch = "109";
+            else if (branch == "220") branch = "110";
+            else if (branch == "222") branch = "111";
+            else if (branch == "223") branch = "112";
+            else if (branch == "230") branch = "113";
+            else if (branch == "238") branch = "114";
+            else if (branch == "239") branch = "115";
+            else if (branch == "241") branch = "116";
+            else if (branch == "242") branch = "117";
+            else if (branch == "244") branch = "119";
+            else if (branch == "245") branch = "120";
+            else if (branch == "246") branch = "121";
+            else if (branch == "247") branch = "122";
+            else if (branch == "248") branch = "123";
+            else if (branch == "250") branch = "124";
+            else if (branch == "254") branch = "125";
+            else if (branch == "260") branch = "126";
+            else if (branch == "261") branch = "127";
+            else if (branch == "262") branch = "128";
+            else if (branch == "263") branch = "129";
+            else if (branch == "264") branch = "130";
+            else if (branch == "265") branch = "131";
+            else if (branch == "266") branch = "132";
+            else if (branch == "267") branch = "133";
+            else if (branch == "268") branch = "134";
+            else if (branch == "274") branch = "135";
+            else if (branch == "277") branch = "136";
+            else if (branch == "289") branch = "137";
+            else if (branch == "290") branch = "138";
+            return branch;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string branch = textBox1.Text.TrimStart().TrimEnd();
+            string position = "10" + textBox2.Text.TrimStart().TrimEnd();
+            branch = convertBranchBareket(branch);
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("C:\\Program Files (x86)\\UltraVNC\\vncviewer.exe", "172.15." + branch + "." + position);
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = procStartInfo;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        private string convertBranchBareket(string branch)
+        {
+            if (branch == "2") branch = "2";
+            else if (branch == "11") branch = "51";
+            else if (branch == "5") branch = "60";
+            else if (branch == "6") branch = "1";
+            else if (branch == "7") branch = "7";
+            else if (branch == "8") branch = "8";
+            else if (branch == "9") branch = "9";
+            else if (branch == "10") branch = "10";
+            else if (branch == "60") branch = "5";
+            else if (branch == "61") branch = "61";
+            else if (branch == "62") branch = "70";
+            else if (branch == "63") branch = "55";
+
+            return branch;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string branch = textBox1.Text.TrimStart().TrimEnd();
+            string position = "10" + textBox2.Text.TrimStart().TrimEnd();
+            branch = convertBranchHCohen(branch);
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("C:\\Program Files (x86)\\UltraVNC\\vncviewer.exe", "172.30." + branch + "." + position);
+            using (Process process = new Process())
+            {
+                process.StartInfo = procStartInfo;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        private string convertBranchHCohen(string branch)
+        {
+            if (branch == "1") branch = "14";
+            else if (branch == "45") branch = "50";
+            else if (branch == "65") branch = "65";
+            else if (branch == "75") branch = "40";
             return branch;
         }
     }
